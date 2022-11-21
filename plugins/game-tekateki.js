7140b84675996d6320f4031a29c7d77f47105c2e
GabutV2 <116761452+GabutV2@users.noreply.github.com>
2022-11-22 05:10:25 +0700

    Update game-tekateki.js

--- plugins/game-tekateki.js
+++ plugins/game-tekateki.js
@@ -22,7 +22,7 @@ ${json.soal}
 
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}htek untuk bantuan
-Bonus: ${poin} XP
+Bonus: ${poin} Money
     `.trim()
     conn.tekateki[id] = [
         await conn.sendButton(m.chat, caption, author, `https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=inferno-logo&doScale=false&scaleWidth=400&scaleHeight=400&fontsize=50&fillTextType=0&backgroundColor=black&text=${command}`, buttons, m),
