74a4ac3245afc8eb7b4c2631a9864cd88ca5f06f
GabutV2 <116761452+GabutV2@users.noreply.github.com>
2022-11-22 05:32:23 +0700

    Update game-tekateki-ans.js

--- plugins/game-tekateki-ans.js
+++ plugins/game-tekateki-ans.js
@@ -1,7 +1,7 @@
 import similarity from 'similarity'
 const threshold = 0.72
 export async function before(m) {
-    let user = db.data.users[m.sender]
+    let users = db.data.users[m.sender]
     if (user.banned) return null
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/Ketik.*htek/i.test(m.quoted.text) || /.*htek/i.test(m.text))
@@ -19,7 +19,7 @@ export async function before(m) {
         let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-            user.money += this.tekateki[id][2]
+            users.money += this.tekateki[id][2]
             conn.sendButton(m.chat, `*Benar!*\n+${this.tekateki[id][2]} Money`, author, null, buttontekateki, m)
             clearTimeout(this.tekateki[id][3])
             delete this.tekateki[id]
